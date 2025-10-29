1c09508733b442c4bc1008146566faba489eba66
SonOfAthena1 <soccerlc0095@gmail.com>
2025-10-29 10:32:51 -0700

    Added some doc comments to methods

--- heap.h
+++ heap.h
@@ -21,6 +21,11 @@ struct MinHeap {
         upheap(size-1, weightArr);
     }
 
+    /**
+     * Switch the root to have the value at the end of the heap, then call downheap()
+     * @param weightArr the array to use for sorting
+     * @return the root value of the heap
+     */
     int pop(int weightArr[]) {
         // remove and return smallest index
         // Replace root with last element, then call downheap()
@@ -37,6 +42,11 @@ struct MinHeap {
         return popValue; // placeholder
     }
 
+    /**
+     * While the child's value in weightArr is less than the parents, swap indices upward
+     * @param pos the index that a weight was inserted into weightArr at
+     * @param weightArr the array to use for sorting the heap
+     */
     void upheap(int pos, int weightArr[]) {
         // swap child upward while smaller than parent
         if (pos <= 0) return;
@@ -51,6 +61,11 @@ struct MinHeap {
         }
     }
 
+    /**
+     * Swap the root downward while it's weightArr value is greater than a child node's
+     * @param pos the index that something was removed from the heap at (should always be 0)
+     * @param weightArr the array to use for sorting
+     */
     void downheap(int pos, int weightArr[]) {
         // swap parent downward while larger than any child
         int currPos = pos;
@@ -79,25 +94,47 @@ struct MinHeap {
         }
     }
 
+    /**
+     * Swap two values in an array
+     * @param parent the index of the first value
+     * @param child the index of the second value
+     */
     void swapData(int parent, int child) {
         int temp = data[parent];
         data[parent] = data[child];
         data[child] = temp;
     }
 
+    /**
+     * Find the index of a parent node given its child's index
+     * @param idx the index of a child node in data[]
+     * @return the index of the parent node in data[]
+     */
     static int getParent(int idx) {
         return (idx - 1) / 2;
     }
 
+    /**
+     * Find the index of a child node in data[] given its parent
+     * @param idx the index of the parent node in data[]
+     * @param child input 1 for left child and 2 for right
+     * @return the index of the child node in data[]
+     */
     static int getChild(int idx, int child) {
         return idx * 2 + child;
     }
 
+    /**
+     * Determine whether a node index is valid for data[]
+     * @param node the index of the node to check
+     * @return 1 if the child exists and 0 if not
+     */
     int exists(int node) const {
         if (node < size) return 1;
         return 0;
     }
 
+    // Override the default printing mechanism for heap
     friend ostream& operator<<(ostream& os, const MinHeap& h) {
         os << "MinHeap: " << endl;
         int count = 0;
@@ -116,6 +153,11 @@ struct MinHeap {
         return os;
     }
 
+    /**
+     * Check if the heap is maintaining proper heap structure
+     * @param freq the array of weights to check if heap is valid
+     * @return a boolean if the heap is valid or not
+     */
     int isValidMinHeap(int freq[]) {
         for (int i = 0; i < size; ++i) {
             int child1 = getChild(i, 1);
